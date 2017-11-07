571961a7930ffd381400dfeff2909ea5f8b92f44
kyle8824 <kyle8824@gmail.com>
2017-11-06 19:50:54 -0500

    Initial commit after starting Mavin project.

---
+++ pet_finder/src/main/java/barnesweb/pet_finder/App.java
@@ -0,0 +1,13 @@
+package barnesweb.pet_finder;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
